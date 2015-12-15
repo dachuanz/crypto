b4c43c8cc1af5a910db35c2263d6b4371a36f1a6
张大川 <dachuanz@gmail.com>
2015-12-15 17:30:32 +0800

    修改

--- src/net/oschina/crypto/AESCoder.java
+++ src/net/oschina/crypto/AESCoder.java
@@ -1,14 +1,25 @@
 package net.oschina.crypto;
 
+import java.security.Key;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 /**
  * 
  * @author zdc52
  *
  */
 public abstract class AESCoder {
+	private static Key toKey(byte[] key) throws Exception {
+		SecretKey secretKey = new SecretKeySpec(key, KEY_ALGORITHM);
+		return secretKey;
+	}
+
 	/**
 	 * 算法定义
 	 */
 	public static final String KEY_ALGORITHM = "AES";
 
+	public static final String CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
 }
