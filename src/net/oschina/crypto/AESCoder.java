439bf65a56aa8bc38aab6bb65d59649b52392282
张大川 <dachuanz@gmail.com>
2015-12-15 17:14:25 +0800

    添加aes算法

---
+++ src/net/oschina/crypto/AESCoder.java
@@ -0,0 +1,14 @@
+package net.oschina.crypto;
+
+/**
+ * 
+ * @author zdc52
+ *
+ */
+public abstract class AESCoder {
+	/**
+	 * À„∑®∂®“Â
+	 */
+	public static final String KEY_ALGORITHM = "AES";
+
+}
