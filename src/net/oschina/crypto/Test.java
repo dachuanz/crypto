bab1ab6e84af74882d19bc90c1323bb9f53a8ed1
张大川 <dachuanz@gmail.com>
2015-12-15 16:04:08 +0800

    xxx

--- src/net/oschina/crypto/Test.java
+++ src/net/oschina/crypto/Test.java
@@ -1,9 +1,10 @@
-package net.oschina.crypto;
+package net.oschina.sql.postgresql;
 
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -12,9 +13,12 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+
 /**
- * 初步实现的加密所发，备用
- * */
+ * 
+ * @author zdc52
+ *
+ */
 public class Test {
 	public static byte[] encrypt(String content, String password) {
 		try {
@@ -23,11 +27,11 @@ public class Test {
 			SecretKey secretKey = kgen.generateKey();
 			byte[] enCodeFormat = secretKey.getEncoded();
 			SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
-			Cipher cipher = Cipher.getInstance("AES");// 设置加密方法
+			Cipher cipher = Cipher.getInstance("AES");// 创建密码器
 			byte[] byteContent = content.getBytes("utf-8");
-			cipher.init(Cipher.ENCRYPT_MODE, key);// 
+			cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
 			byte[] result = cipher.doFinal(byteContent);
-			return result; // 
+			return result; // 加密
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		} catch (NoSuchPaddingException e) {
@@ -43,4 +47,44 @@ public class Test {
 		}
 		return null;
 	}
+	
+	public static byte[] encrypt1(String content, String password) {
+		try {
+			KeyGenerator kgen = KeyGenerator.getInstance("AES");
+			kgen.init(128, new SecureRandom(password.getBytes()));
+			SecretKey secretKey = kgen.generateKey();
+			byte[] enCodeFormat = secretKey.getEncoded();
+			SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+			Cipher cipher = Cipher.getInstance("Blowfish");// 创建密码器
+			byte[] byteContent = content.getBytes("utf-8");
+			cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
+			byte[] result = cipher.doFinal(byteContent);
+			return result; // 加密
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		String asB64 = Base64.getEncoder().encodeToString("some string".getBytes("utf-8"));
+		System.out.println(asB64); // 输出为: c29tZSBzdHJpbmc=
+		byte[] asBytes = Base64.getDecoder().decode("c29tZSBzdHJpbmc=");
+		System.out.println(new String(asBytes, "utf-8")); // 输出为: some string
+
+	}
+
+	// 解码
+
 }
