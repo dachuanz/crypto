65e8867cb1d41f7cba1c97a570038719cc140129
张大川 <dachuanz@gmail.com>
2015-12-15 18:08:36 +0800

    添加

--- src/net/oschina/crypto/AESCoder.java
+++ src/net/oschina/crypto/AESCoder.java
@@ -1,7 +1,10 @@
 package net.oschina.crypto;
 
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -11,9 +14,42 @@ import javax.crypto.spec.SecretKeySpec;
  *
  */
 public abstract class AESCoder {
+
+	public static byte[] initKey() throws NoSuchAlgorithmException {
+		KeyGenerator kg = KeyGenerator.getInstance(KEY_ALGORITHM);
+		kg.init(256);
+		SecretKey secretKey = kg.generateKey();
+		return secretKey.getEncoded();
+
+	}
+	
+	public static byte [] encypt (byte[] data, byte[] key) throws Exception
+	{
+		Key key2 = toKey(key);
+		Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+		cipher.init(Cipher.ENCRYPT_MODE, key2);
+		return cipher.doFinal(data);
+		
+		
+	}
+
 	private static Key toKey(byte[] key) throws Exception {
 		SecretKey secretKey = new SecretKeySpec(key, KEY_ALGORITHM);
 		return secretKey;
+	}
+
+	/**
+	 * 
+	 * @param data
+	 * @param key
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] decypt(byte[] data, byte[] key) throws Exception {
+		Key key2 = toKey(key);
+		Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+		cipher.init(Cipher.DECRYPT_MODE, key2);
+		return cipher.doFinal(data);
 	}
 
 	/**
---
+++ src/net/oschina/crypto/AESTest1.java
@@ -0,0 +1,19 @@
+package net.oschina.crypto;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class AESTest1 {
+	public static void main(String[] args) throws Exception {
+		String string = "ZDC524";
+		byte[] bs = string.getBytes();
+		System.err.println("原文" + string);
+		byte[] bs2 = AESCoder.initKey();
+		System.err.println("密码" + Base64.getEncoder().encodeToString(bs2));
+		bs = AESCoder.encypt(bs, bs2);
+		System.err.println("加密后" + Base64.getEncoder().encodeToString(bs));
+		byte[] bs3 = AESCoder.decypt(bs, bs2);
+		String string2 = new String(bs3);
+		System.err.println("解密后" + string2);
+	}
+}
