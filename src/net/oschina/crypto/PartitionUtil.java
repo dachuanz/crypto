28a21c1a6fbb5d2ce5b7b82fe2d04337c8d45ab7
张大川 <dachuanz@gmail.com>
2016-01-26 12:23:19 +0800

    init

---
+++ src/net/oschina/crypto/PartitionUtil.java
@@ -0,0 +1,33 @@
+package net.oschina.crypto;
+
+import java.util.UUID;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.Conversion;
+
+public class PartitionUtil {
+	/**
+	 * 
+	 * @param id
+	 * @param i
+	 * @return
+	 */
+	public static Long partitioning(String id, int i) {
+
+		byte[] bs = DigestUtils.sha1(id);
+
+		UUID long1 = Conversion.byteArrayToUuid(bs, 0);
+		double d = Math.pow(2, i);
+
+		Long long2 = Math.round(d);
+
+		Long l = (long) (long1.getLeastSignificantBits() % long2);
+		if (l < 0L) {
+			l = (long) (l + long2 - 1);
+
+		}
+
+		return l;
+	}
+
+}
