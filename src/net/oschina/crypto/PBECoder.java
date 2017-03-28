6d5ab0af39f2d1cd79a1f7ecaddc07f29e4632bc
张大川 <dachuanz@gmail.com>
2017-03-28 13:55:28 +0800

    1111

--- src/net/oschina/crypto/PBECoder.java
+++ src/net/oschina/crypto/PBECoder.java
@@ -2,10 +2,10 @@ package net.oschina.crypto;
 
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -13,75 +13,70 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.PBEParameterSpec;
+
 /**
  * 
  * @author 张大川
- * dachuanz@gmail.com
- *基于口令的加密
+ *
  */
-public abstract class PBECoder {
-	public static final String ALGORITHM = "PBEWITHMD5andDES";
-	public static final int ITERATION_COUNT = 100;
+public class PBECoder {
+
+	private static final String ENCRYPTION_ALGORITHM = "PBEWithSHA1AndRC2_128";
+
+	private static final int ITERATIONS = 1000;
 
 	/**
-	 * 生成密码
+	 * 
+	 * @param encodedEncryptedSecret
 	 * @param password
+	 * @param salt
+	 * @param iv
 	 * @return
 	 * @throws NoSuchAlgorithmException
 	 * @throws InvalidKeySpecException
+	 * @throws NoSuchPaddingException
+	 * @throws InvalidKeyException
+	 * @throws InvalidAlgorithmParameterException
+	 * @throws IllegalBlockSizeException
+	 * @throws BadPaddingException
 	 */
-	private static Key toKey(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
-		PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray());
+	public static byte[] decodeAndDecryptSecret(String encodedEncryptedSecret, char[] password, byte[] salt, byte[] iv)
+			throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException,
+			InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
 
-		SecretKeyFactory factory = SecretKeyFactory.getInstance(ALGORITHM);
+		PBEKeySpec keySpec = new PBEKeySpec(password);
+		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(ENCRYPTION_ALGORITHM);
+		SecretKey secretKey = keyFactory.generateSecret(keySpec);
+		PBEParameterSpec parameterSpec = new PBEParameterSpec(salt, ITERATIONS, new IvParameterSpec(iv));
 
-		SecretKey key = factory.generateSecret(keySpec);
-		return key;
+		Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+		cipher.init(Cipher.DECRYPT_MODE, secretKey, parameterSpec);
+
+		return cipher.doFinal(Base64.getDecoder().decode(encodedEncryptedSecret));
+
 	}
 
-	/**
-	 * 生成盐
-	 * 
-	 * @return
-	 */
 	public static byte[] initSalt() {
 		SecureRandom random = new SecureRandom();
 
 		return random.generateSeed(8);
 	}
 
-	/**
-	 * 加密
-	 * @param data
-	 * @param salt
-	 * @param password
-	 * @return
-	 * @throws NoSuchAlgorithmException
-	 * @throws InvalidKeySpecException
-	 * @throws NoSuchPaddingException
-	 * @throws InvalidKeyException
-	 * @throws InvalidAlgorithmParameterException
-	 * @throws IllegalBlockSizeException
-	 * @throws BadPaddingException
-	 */
-	public static byte[] encypt(byte[] data, byte[] salt, String password)
-			throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException,
-			InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+	public static byte[] initIV() {
+		SecureRandom random = new SecureRandom();
 
-		Key key = toKey(password);
-		PBEParameterSpec parameterSpec = new PBEParameterSpec(salt, ITERATION_COUNT);
-		Cipher cipher = Cipher.getInstance(ALGORITHM);
-		cipher.init(Cipher.ENCRYPT_MODE, key, parameterSpec);
-		return cipher.doFinal(data);
+		return random.generateSeed(16);
 	}
 
 	/**
-	 * 解密
-	 * @param data
-	 * @param salt
+	 * 
+	 * @param secret
 	 * @param password
+	 * @param salt
+	 * @param iv
 	 * @return
 	 * @throws NoSuchAlgorithmException
 	 * @throws InvalidKeySpecException
@@ -91,14 +86,20 @@ public abstract class PBECoder {
 	 * @throws IllegalBlockSizeException
 	 * @throws BadPaddingException
 	 */
-	public static byte[] decypt(byte[] data, byte[] salt, String password)
+	public static String encryptAndEncodeSecret(byte[] secret, char[] password, byte[] salt, byte[] iv)
 			throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException,
 			InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
 
-		Key key = toKey(password);
-		PBEParameterSpec parameterSpec = new PBEParameterSpec(salt, ITERATION_COUNT);
-		Cipher cipher = Cipher.getInstance(ALGORITHM);
-		cipher.init(Cipher.DECRYPT_MODE, key, parameterSpec);
-		return cipher.doFinal(data);
+		PBEKeySpec keySpec = new PBEKeySpec(password, salt, ITERATIONS);
+		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(ENCRYPTION_ALGORITHM);
+		SecretKey secretKey = keyFactory.generateSecret(keySpec);
+		PBEParameterSpec parameterSpec = new PBEParameterSpec(salt, ITERATIONS, new IvParameterSpec(iv));
+
+		Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+		cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
+
+		byte[] encryptedSecret = cipher.doFinal(secret);
+		return Base64.getEncoder().encodeToString(encryptedSecret);
+
 	}
 }
