1118037373444e9b9389f72b25add730c56587b7
张大川 <dachuanz@gmail.com>
2017-03-31 11:26:33 +0800

    22

--- src/net/oschina/crypto/AES_ECB_Coder.java
+++ src/net/oschina/crypto/AES_ECB_Coder.java
@@ -14,10 +14,24 @@ import javax.crypto.spec.SecretKeySpec;
 
 /**
  * 
- * @author 
- * @version 1.0
+ * ECB模式 优点:
+ * 
+ * 1.简单；
+ * 
+ * 2.有利于并行计算；
+ * 
+ * 3.误差不会被传送；
+ * 
+ * 缺点:
+ * 
+ * 1.不能隐藏明文的模式；
+ * 
+ * 2.可能对明文进行主动攻击；
  */
 public abstract class AES_ECB_Coder {
+	public static final String KEY_ALGORITHM = "AES";
+
+	public static final String CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
 
 	public static byte[] initKey(int i) throws NoSuchAlgorithmException {
 		KeyGenerator kg = KeyGenerator.getInstance(KEY_ALGORITHM);
@@ -28,7 +42,7 @@ public abstract class AES_ECB_Coder {
 	}
 
 	public static byte[] encypt(byte[] data, byte[] key) throws NoSuchAlgorithmException, NoSuchPaddingException,
-			InvalidKeyException, IllegalBlockSizeException, BadPaddingException  {
+			InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		Key key2 = toKey(key);
 		Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
 		cipher.init(Cipher.ENCRYPT_MODE, key2);
@@ -36,20 +50,17 @@ public abstract class AES_ECB_Coder {
 
 	}
 
-	private static Key toKey(byte[] key)  {
+	private static Key toKey(byte[] key) {
 		SecretKey secretKey = new SecretKeySpec(key, KEY_ALGORITHM);
 		return secretKey;
 	}
 
 	public static byte[] decypt(byte[] data, byte[] key) throws NoSuchAlgorithmException, NoSuchPaddingException,
-			InvalidKeyException, IllegalBlockSizeException, BadPaddingException  {
+			InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		Key key2 = toKey(key);
 		Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
 		cipher.init(Cipher.DECRYPT_MODE, key2);
 		return cipher.doFinal(data);
 	}
 
-	public static final String KEY_ALGORITHM = "AES";
-
-	public static final String CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
 }
